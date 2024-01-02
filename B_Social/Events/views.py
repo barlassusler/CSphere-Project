6c0febcda7c702655c2d1253dcbd908519e86de3
barlassusler <suslerbarlas11@gmail.com>
2024-01-02 19:05:42 +0300

    last_version_with_css

--- B_Social/Events/views.py
+++ B_Social/Events/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import EventApplicationForm
 
-# Create your views here.
+
 def view(request):
     return render(request, 'Events.html')
 
+
 def view2(request):
-    return render(request,'camping.html')
+    return render(request, 'camping.html')
+
+
+def event_application(request):
+    if request.method == 'POST':
+        form = EventApplicationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            return redirect('success_page')
+    else:
+        form = EventApplicationForm()
+
+    return render(request, 'events/event_application_form.html', {'form': form})

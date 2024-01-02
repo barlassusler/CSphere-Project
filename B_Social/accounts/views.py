94d3869f9461f4db34169e0b1d564fdb9b25f3e4
kardakyol <92306169+kardakyol@users.noreply.github.com>
2024-01-02 23:41:53 +0300

    signup login geliştirme

--- B_Social/accounts/views.py
+++ B_Social/accounts/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login as auth_login
 from .forms import SignUpForm, SignInForm
+from django.contrib.auth import authenticate
 
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
-            return redirect('login')
-
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            if user is not None:
+                auth_login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+                # Kullanıcı başarıyla kayıt oldu, şimdi login sayfasına yönlendir
+                return redirect('login')  # 'login' yerine kendi login sayfanızın adını kullanın
+            else:
+                # Kullanıcı doğrulanamadı, isteğe bağlı olarak bir hata mesajı verebilirsiniz.
+                return render(request, 'signup.html', {'form': form, 'error_message': 'Kullanıcı doğrulanamadı'})
     else:
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
@@ -18,7 +27,7 @@ def login(request):
         form = SignInForm(data=request.POST)
         if form.is_valid():
             user = form.get_user()
-            login(request, user)
+            auth_login(request, user)
             return redirect('home')
     else:
         form = SignInForm()
